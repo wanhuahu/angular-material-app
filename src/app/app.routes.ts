52031508d766d68becdcbabe120b370ca4999d2e
Wanhua Hu <wanhuahu@gmail.com>
2025-04-19 01:22:19 -0400

    app.routes.ts format

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,6 @@ import { AccountComponent } from './account/account.component';
 import { TodoComponent } from './todo/todo.component';
 
 export const routes: Routes = [
-    { path: 'account', component: AccountComponent },
-    { path: 'todo', component: TodoComponent }
-  ];
+  { path: 'account', component: AccountComponent },
+  { path: 'todo', component: TodoComponent }
+];
