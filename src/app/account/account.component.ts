e7745db9a059f8e3020a987a1fff829d3e8e8b08
Wanhua Hu <wanhuahu@gmail.com>
2025-04-19 00:01:10 -0400

    account works

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { AngularMaterialModule } from '../../shared/material.module';
 
 @Component({
   selector: 'app-account',
-  imports: [],
+  imports: [
+    AngularMaterialModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './account.component.html',
   styleUrl: './account.component.scss'
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit {
+  public formGroup!: FormGroup;
+  hidePassword = true;
 
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[^@\\s]+@[^\\s@]+.[^\\s@]+$'),
+        ],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  public login(): void {
+    const email = this.formGroup.controls['email'].value;
+    const password = this.formGroup.controls['password'].value;
+  }
 }
