64e225dc74807ef3d6299cf1be587efb162f700b
Wanhua Hu <wanhuahu@gmail.com>
2025-04-19 00:09:09 -0400

    generate component todo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { AccountComponent } from './account/account.component';
+import { TodoComponent } from './todo/todo.component';
 
 export const routes: Routes = [
     { path: 'account', component: AccountComponent },
+    { path: 'todo', component: TodoComponent }
   ];
